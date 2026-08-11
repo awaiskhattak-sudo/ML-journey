8ab2b4bb1e29a9f0da8ca58f483a6e1165451728
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-12 00:08:52 +0500

    Basic arithmetic operations in operators.py

---
+++ operators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+
+total = a + b
+difference = a - b
+multiplication = a * b
+division = a / b
+floor_division = a // b
+power = a ** b
+remainder = a % b
+
+print(total)
+print(difference)
+print(multiplication)
+print(division)
+print(floor_division)
+print(power)
+print(remainder)
