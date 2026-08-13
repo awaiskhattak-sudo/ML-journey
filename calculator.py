00f42be450a4296a1e5c01ed8f46b480f81030cd
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-13 17:16:45 +0500

    basic calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+division = num1 / num2
+multiply = num1 * num2
+power = num1 ** num2
+
+print(f"Addition: {sum}")
+print(f"Subtraction: {difference}")
+print(f"Division: {division}")
+print(f"Multiplication: {multiply}")
+print(f"Power: {power}")
