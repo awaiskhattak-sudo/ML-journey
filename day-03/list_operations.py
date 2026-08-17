cef329537fa5684c612de1e10dd6b1c8d0ee1fe2
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:17:25 +0500

    list_operations.py with basic list manipulations
    Implement basic list operations including modification, appending, and deletion.

---
+++ day-03/list_operations.py
@@ -0,0 +1,18 @@
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers)
+print(numbers[0])
+print(numbers[4])
+
+numbers[2] = 35
+print(numbers)
+
+numbers.append(60)
+print(numbers)
+
+del numbers[1]
+
+print(40 in numbers)
+print(len(numbers))
+
+print(numbers)
