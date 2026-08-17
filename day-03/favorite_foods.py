a573aede3c6bdd51210358e402b04827983a8c01
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:14:52 +0500

    favorite_foods.py to manage a food list

---
+++ day-03/favorite_foods.py
@@ -0,0 +1,13 @@
+foods = ["pizza", "burger", "biryani", "pasta"]
+
+print(foods)
+print(foods[0])
+
+foods[1] = "shawarma"
+print(foods)
+
+foods.append("kabab")
+print(foods)
+
+foods.remove("pasta")
+print(foods)
