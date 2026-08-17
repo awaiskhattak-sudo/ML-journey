fc6bc47f439a6de595e7224c4a69f826b5a1deca
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:14:10 +0500

    shopping list functionality in shopping_list.py

---
+++ day-03/shopping_list.py
@@ -0,0 +1,13 @@
+shopping = ["milk", "bread", "eggs", "rice"]
+
+print(shopping)
+
+shopping.append("juice")
+print(shopping)
+
+shopping.remove("bread")
+print(shopping)
+
+print("eggs" in shopping)
+
+print(len(shopping))
