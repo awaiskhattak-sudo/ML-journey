cb5dee220a0265a588396afb698dd631f51c293f
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:13:14 +0500

    student_marks.py to manage student scores

---
+++ day-03/student_marks.py
@@ -0,0 +1,16 @@
+marks = [85, 95, 98, 77, 79]
+
+print(marks)
+print(marks[0])
+print(marks[4])
+
+marks[3] = 87
+print(marks)
+
+marks.append(89)
+print(marks)
+
+marks.pop(2)
+print(marks)
+
+print(len(marks))
