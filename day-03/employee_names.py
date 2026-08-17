23611bb2397ca1d929f71a51a896c29ca6bafaf5
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:16:11 +0500

    employee_names.py to manage employee list

---
+++ day-03/employee_names.py
@@ -0,0 +1,15 @@
+employees = ["Ali", "Ahmed", "Usman", "Hamza", "Bilal"]
+
+print(employees)
+print(employees[0])
+print(employees[4])
+
+employees.append("Zain")
+print(employees)
+
+employees.remove("Ahmed")
+print(employees)
+
+print("Ali" in employees)
+
+print(len(employees))
