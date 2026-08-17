ac89b39587af6c10363cd35c2abfe3e526cdf3a1
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:16:51 +0500

    student information and update grades
    Initialize student information and update grades

---
+++ day-03/student_information.py
@@ -0,0 +1,12 @@
+student = ["Awais", 22, 85, "B"]
+
+print(student[0])
+print(student[1])
+print(student[2])
+print(student[3])
+
+student[2] = 90
+print(student)
+
+student[3] = "A"
+print(student)
