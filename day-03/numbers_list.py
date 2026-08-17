9e27616c44b31b47cae0c79168d7681a6734477c
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-17 17:15:33 +0500

    numbers_list.py with list operations

---
+++ day-03/numbers_list.py
@@ -0,0 +1,15 @@
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers)
+print(numbers[0])
+print(numbers[2])
+print(numbers[4])
+
+numbers[2] = 35
+numbers.append(60)
+print(numbers)
+
+numbers.remove(20)
+print(numbers)
+
+print(len(numbers))
