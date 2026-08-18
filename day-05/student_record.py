3a34fa9813f17e9c4339a09e27bbdec5bb968a16
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-18 21:40:53 +0500

    student record and display function

---
+++ day-05/student_record.py
@@ -0,0 +1,18 @@
+student = {
+    "name": "Awais",
+    "age": 22,
+    "marks": 85
+}
+
+def display_student(student):
+
+    name = student["name"]
+    age = student["age"]
+    marks = student["marks"]
+
+    print("Name:", name)
+    print("Age:", age)
+    print("Marks:", marks)
+
+
+display_student(student)
