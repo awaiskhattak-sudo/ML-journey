35f54f06ca81bc93d7e871af713f81c8f77e0342
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-18 16:31:07 +0500

    highest_marks.py to find top student

---
+++ day-04/highest_marks.py
@@ -0,0 +1,17 @@
+students = {
+    "Ali": 80,
+    "Ahmed": 65,
+    "Sara": 90,
+    "Ayesha": 75
+}
+
+highest_marks = 0
+top_student = ""
+
+for student, marks in students.items():
+    if marks > highest_marks:
+        highest_marks = marks
+        top_student = student
+
+print("Highest marks:", highest_marks)
+print("Top student:", top_student)
