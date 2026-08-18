78dd31a16d2b0c9f9f165b95bdef79c963e2e2e8
awaiskhattak-sudo <awaiskhattak668899@gmail.com>
2026-08-18 16:26:58 +0500

    data statistics calculations for temperatures

---
+++ day-04/practical_01_data_statistics.py
@@ -0,0 +1,10 @@
+temperatures = [32.5, 34.2, 31.8, 35.0, 33.6]
+
+print(sum(temperatures))
+print(max(temperatures))
+print(len(temperatures))
+print(min(temperatures))
+
+average = sum(temperatures) / len(temperatures)
+
+print(average)
